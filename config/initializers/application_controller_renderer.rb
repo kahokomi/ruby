213e1d10149bde36712b166e89828ab369198b6f
EC2 Default User <knk61721@gmail.com>
2020-10-22 11:33:21 +0000

    [start]rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '017f8de96e8343a2723f5bbcfc701432d034c022dceb78d55fdce050afed865465a5434d70ff11e4ef38052b1713a1a74db31ecf870465938d0aeca59fa5f5aa'
